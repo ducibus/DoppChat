388a89dc7cde178563023fb0bec8266682edbb9b
Kush Patel <kush@kush.in>
2022-03-26 16:34:49 +0530

    tidying up code

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -10,7 +10,8 @@
 		Timestamp
 	} from 'firebase/firestore';
 	import type { QueryDocumentSnapshot, DocumentData } from 'firebase/firestore';
-	import { newError } from './errorStore';
+
+	// making sure only logged in users can see
 	firebaseUser.subscribe(
 		(user) => {
 			if (!user) window.location.reload();
@@ -19,15 +20,8 @@
 			console.log(error);
 		}
 	);
-	async function getDopps() {
-		const doppQuery = query(
-			collection(db, 'dopps'),
-			where('owner', '==', $firebaseUser?.uid),
-			orderBy('created_at', 'desc')
-		);
-		const doppsData = await getDocs(doppQuery);
-		return doppsData.docs;
-	}
+
+	// utils
 	async function hash(string: string) {
 		const utf8 = new TextEncoder().encode(string);
 		const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
@@ -38,6 +32,9 @@
 	function generateId() {
 		return hash((Math.random() + 1).toString(36).substring(7) + Date.now().toString);
 	}
+
+	// managing dopps
+	let newDoppName: string;
 	async function createNewDopp() {
 		await addDoc(collection(db, 'dopps'), {
 			name: newDoppName,
@@ -48,20 +45,29 @@
 		window.location.reload();
 	}
 	let doppsPromise = getDopps();
+	async function getDopps() {
+		const doppQuery = query(
+			collection(db, 'dopps'),
+			where('owner', '==', $firebaseUser?.uid),
+			orderBy('created_at', 'desc')
+		);
+		const doppsData = await getDocs(doppQuery);
+		return doppsData.docs;
+	}
+
+	// boolean to check if the list of cards is long enough to be scrolled
 	let long: boolean;
-	let newDoppName: string;
 	doppsPromise.then((dopps) => {
 		long = dopps.length > 4;
 	});
-	const getShareData = (dopp: QueryDocumentSnapshot<DocumentData>) => {
-		return {
+
+	// card stuff
+	const share = (dopp: QueryDocumentSnapshot<DocumentData>) => {
+		const shareData = {
 			title: 'Dopp',
 			text: 'You have been invited to a dopp',
 			url: `https://doppchat.ml/dopps/${dopp.data().dopp}`
 		};
-	};
-	const share = (dopp: QueryDocumentSnapshot<DocumentData>) => {
-		const shareData = getShareData(dopp);
 		navigator.share(shareData);
 	};
 	let next = false;
@@ -70,88 +76,89 @@
 	};
 </script>
 
-<div class="dashboard">
-	<button class="btn-logout" type="button" on:click={logout}>Log out</button>
-	<h1>Hi {$firebaseUser?.displayName}</h1>
-	<h2>Here are your <strong>Dopps</strong></h2>
-	{#await doppsPromise}
-		<p>Getting your Dopps . . .</p>
-	{:then dopps}
-		<ul class:justify={long === false}>
-			{#each dopps as dopp}
-				<li>
-					<header>
-						<h1>{dopp.data().name}</h1>
-					</header>
-					<a
-						class="dopp_btn"
-						href="https://doppchat.ml/dopps/{dopp.data().dopp}"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="45"
-							height="45"
-							fill="currentColor"
-							class="bi bi-box-arrow-right"
-							viewBox="0 0 16 16"
-						>
-							<path
-								fill-rule="evenodd"
-								d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
-							/>
-							<path
-								fill-rule="evenodd"
-								d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
-							/>
-						</svg>
-						<h3>Open</h3>
-					</a>
-					<button
-						class="dopp_btn"
-						on:click={() => {
-							share(dopp);
-						}}
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							height="45"
-							width="45"
-							viewBox="0 0 20 20"
-							fill="currentColor"
-						>
-							<path
-								d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
-							/>
-						</svg>
-						<h3>Share</h3>
-					</button>
-				</li>
-			{/each}
+<button class="btn-logout" type="button" on:click={logout}>Log out</button>
+<h1>Hi {$firebaseUser?.displayName}</h1>
+<h2>Here are your <strong>Dopps</strong></h2>
+{#await doppsPromise}
+	<p>Getting your Dopps . . .</p>
+{:then dopps}
+	<ul class:justify={long === false}>
+		{#each dopps as dopp}
 			<li>
-				<div class="create" class:hide={next === true} on:click={openNext}>
-					<svg height="98" width="98" viewBox="0 0 20 20" fill="currentColor">
+				<header>
+					<h1>{dopp.data().name}</h1>
+				</header>
+				<a
+					class="dopp_btn"
+					href="https://doppchat.ml/dopps/{dopp.data().dopp}"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="45"
+						height="45"
+						fill="currentColor"
+						class="bi bi-box-arrow-right"
+						viewBox="0 0 16 16"
+					>
 						<path
 							fill-rule="evenodd"
-							d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-							clip-rule="evenodd"
+							d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"
+						/>
+						<path
+							fill-rule="evenodd"
+							d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
 						/>
 					</svg>
-				</div>
-				<div class:hide={next === false}>
-					<input placeholder="Name" type="text" bind:value={newDoppName} />
-					<button on:click={createNewDopp}>Create</button>
-				</div>
+					<h3>Open</h3>
+				</a>
+				<button
+					class="dopp_btn"
+					on:click={() => {
+						share(dopp);
+					}}
+				>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="45"
+						width="45"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+					>
+						<path
+							d="M15 8a3 3 0 10-2.977-2.63l-4.94 2.47a3 3 0 100 4.319l4.94 2.47a3 3 0 10.895-1.789l-4.94-2.47a3.027 3.027 0 000-.74l4.94-2.47C13.456 7.68 14.19 8 15 8z"
+						/>
+					</svg>
+					<h3>Share</h3>
+				</button>
 			</li>
-		</ul>
-	{:catch error}
-		<p>You have no dopps</p>
-		<p>{error}</p>
-	{/await}
-</div>
+		{/each}
+		<li>
+			<div class="create" class:hide={next === true} on:click={openNext}>
+				<svg height="98" width="98" viewBox="0 0 20 20" fill="currentColor">
+					<path
+						fill-rule="evenodd"
+						d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+						clip-rule="evenodd"
+					/>
+				</svg>
+			</div>
+			<div class:hide={next === false}>
+				<input placeholder="Name" type="text" bind:value={newDoppName} />
+				<button on:click={createNewDopp}>Create</button>
+			</div>
+		</li>
+	</ul>
+{:catch error}
+	<p>Oops! I broke 💩</p>
+	<p>{error}</p>
+{/await}
 
 <style>
+	h1 {
+		margin: 4rem;
+	}
 	ul {
 		list-style: none;
 		display: flex;
@@ -165,12 +172,21 @@
 		margin: auto;
 		width: 100%;
 	}
+	ul::-webkit-scrollbar {
+		width: 10px;
+		height: 5px;
+	}
+	ul::-webkit-scrollbar-thumb {
+		background: #201c29;
+		border-radius: 10px;
+		box-shadow: inset 2px 2px 2px hsla(0, 0%, 100%, 0.25), inset -2px -2px 2px rgba(0, 0, 0, 0.25);
+	}
+
+	ul::-webkit-scrollbar-track {
+		background-color: #9446e2;
+	}
 	.justify {
 		justify-content: center;
-	}
-	input {
-		width: 150px;
-		margin: 20px;
 	}
 	li {
 		height: 350px;
@@ -218,18 +234,9 @@
 		display: none;
 		transition: 0.5s;
 	}
-	ul::-webkit-scrollbar {
-		width: 10px;
-		height: 5px;
-	}
-	ul::-webkit-scrollbar-thumb {
-		background: #201c29;
-		border-radius: 10px;
-		box-shadow: inset 2px 2px 2px hsla(0, 0%, 100%, 0.25), inset -2px -2px 2px rgba(0, 0, 0, 0.25);
-	}
-
-	ul::-webkit-scrollbar-track {
-		background-color: #9446e2;
+	input {
+		width: 150px;
+		margin: 20px;
 	}
 	.dopp_btn {
 		width: 250px;
@@ -253,16 +260,6 @@
 	}
 	.dopp_btn:hover h3 {
 		display: block;
-	}
-	.dashboard {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-		background-color: #101827;
-		color: #e9f1f7;
-		text-align: center;
-	}
-	h1 {
-		margin: 4rem;
 	}
 	.btn-logout {
 		background-color: #9446e2;
